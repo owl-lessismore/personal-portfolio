0ac2e1ef14c891cf12a4287f13534fc455245919
Darwin <serocefinodarwin@gmail.com>
2022-07-12 20:32:41 +0800

    initial design home page

---
+++ javascript/vanta_birdAnimation.js
@@ -0,0 +1,19 @@
+let animationContainer = document.querySelector("#home-page-animation");
+
+VANTA.BIRDS({
+el: animationContainer,
+mouseControls: true,
+touchControls: true,
+gyroControls: false,
+minHeight: 200.00,
+minWidth: 200.00,
+scale: 1.00,
+scaleMobile: 1.00,
+backgroundColor: 0x7e9880,
+color1: 0xa47575,
+color2: 0x5398af,
+colorMode: "lerp",
+birdSize: 1.70,
+speedLimit: 8.00,
+quantity: 3.00
+});
