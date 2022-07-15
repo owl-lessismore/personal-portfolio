0ecce0a1677077ce3d9c462d56af87c61db73100
Darwin <serocefinodarwin@gmail.com>
2022-07-16 02:47:09 +0800

    carousel skills

--- javascript/skills.js
+++ javascript/skills.js
@@ -2,35 +2,50 @@ const burgerTop = document.querySelector('[data-burger-top]')
 const burgerMiddle = document.querySelector('[data-burger-middle]')
 const burgerBottom = document.querySelector('[data-burger-bottom]')
 
-burgerTop.addEventListener('click', () => {
+let skillsCarousel = tns({
+    "container": ".skills-slider",
+    "items": 1,
+    "loop": false,
+    "speed": 400,
+    "nav": false,
+    "controls": false,
+    "mouseDrag": true,
+    "autoWidth": true
+})
 
+burgerTop.addEventListener('click', () => {
     burgerTop.classList.toggle('burger-top')
 
     if(burgerTop.classList.contains('burger-top')) {
         burgerTop.setAttribute('src','/assets/burgerColoredTop.png')
+        skillsCarousel.goTo(0)
     } else {
-        burgerTop.setAttribute('src','/assets/burgerDrawingTop.png')   
+        burgerTop.setAttribute('src','/assets/burgerDrawingTop.png')
+        skillsCarousel.goTo(0)
     }
 })
 
 burgerMiddle.addEventListener('click', () => {
-
     burgerMiddle.classList.toggle('burger-middle')
 
     if(burgerMiddle.classList.contains('burger-middle')) {
         burgerMiddle.setAttribute('src','/assets/burgerColoredMiddle.png')
+        skillsCarousel.goTo(1)  
     } else {
-        burgerMiddle.setAttribute('src','/assets/burgerDrawingMiddle.png')   
+        burgerMiddle.setAttribute('src','/assets/burgerDrawingMiddle.png')  
+        skillsCarousel.goTo(1)
     }
 })
 
 burgerBottom.addEventListener('click', () => {
-
     burgerBottom.classList.toggle('burger-bottom')
-
+    
     if(burgerBottom.classList.contains('burger-bottom')) {
         burgerBottom.setAttribute('src','/assets/burgerColoredBottom.png')
+        skillsCarousel.goTo(2)
     } else {
-        burgerBottom.setAttribute('src','/assets/burgerDrawingBottom.png')   
+        burgerBottom.setAttribute('src','/assets/burgerDrawingBottom.png') 
+        skillsCarousel.goTo(2)  
     }
 })
+
