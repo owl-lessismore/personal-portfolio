06e49871ed81812ee3558ad6d90ab044b0cc1f6f
Darwin <serocefinodarwin@gmail.com>
2022-07-20 14:37:48 +0800

    close navbar when click and click outside

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,17 +1,17 @@
 const toggleNavbar = document.getElementsByClassName('fa-bars')[0]
 const navbarMenu = document.getElementsByClassName('navbar-links')[0]
 
-
 toggleNavbar.addEventListener('click',() => {
     navbarMenu.classList.toggle('active')
 })
 
 
+window.addEventListener('click', (event) => {
+    if(event.target.id !== toggleNavbar.id && event.target.id !== navbarMenu.id){
+        navbarMenu.classList.remove('active')
+    }
+})
 
-document.addEventListener("click", () => {
-    if (navbarMenu.classList.contains("active")) {
-        navbarMenu.classList.remove("active")
-        }
-}, true);
-  
+
+
   
